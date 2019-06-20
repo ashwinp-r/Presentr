045abd4b5672349cc96ef2a456b358289aa630ba
Ashwin R <ashwinr14@gmail.com>
2019-06-20 14:54:15 -0700

    Add black to background color

--- Presentr/AlertViewController.swift
+++ Presentr/AlertViewController.swift
@@ -188,6 +188,7 @@ public class AlertViewController: UIViewController, CornerRadiusSettable {
 
 	private func setupContainerView() {
 		containerView.clipsToBounds = true
+        containerView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.6)
 	}
 
     private func setupFonts() {
